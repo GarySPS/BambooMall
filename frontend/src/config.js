74028ce7ff30d7f96b8be79cbd79e065280074d6
GarySPS <layngelngel96@gmail.com>
2025-07-21 00:25:23 +0630

    fix: update API base URL config and add .env for production

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,2 @@
-//export const API_BASE_URL =
-  //process.env.REACT_APP_API_URL || "http://localhost:4000/api";
 export const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "https://bamboomall-backend.onrender.com/api";
+  process.env.REACT_APP_API_BASE_URL || "https://bamboomall-backend.onrender.com/api";
